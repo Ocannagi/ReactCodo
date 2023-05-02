c73078528ae3ba2d57a4d5450115d7b9ec5a2c16
Nicolás Gómez Ivaldi <nicoivaldi@gmail>
2023-05-01 23:33:57 -0300

    cambio menor

--- src/Componentes/Imgmeme.js
+++ src/Componentes/Imgmeme.js
@@ -13,8 +13,10 @@ const Imgmeme = ()=>{
         <div>
             <h1 className='mt-5 mb-3 text-light'>Edita tu propio meme</h1>
 
-            <h1 className='mt-2 mb-3'>Escribi tu frase</h1>
+            <h2 className='mt-2 mb-3'>Escribi tu frase</h2>
             <input onChange={textomeme} className='form-control w-25' type='text' placeholder='Pone tu frase' name='meme'/>
+
+            <h2 className='mt-2 mb-3'>Escribi tu frase</h2>
 
 
 
